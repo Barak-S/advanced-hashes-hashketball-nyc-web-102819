2d096cfcc0b80a62c848e531d448e5b4ca1f44b9
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-26 20:42:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,13 +153,13 @@ def player_numbers(team_name)
   return output
 end
 
-def player_stats(input)
+def player_stats(player_name)
   game_hash.each do |team, team_info|
     team_info.each do |key, value|
       if key == :players
         value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
+          if player_name == player[:player_name]
+            player.delete(:player_name) 
             return player
           end
         end
