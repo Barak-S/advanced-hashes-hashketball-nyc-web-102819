df19d8a6b25c01c84860f1e3bef9c4db7fd333ff
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-26 20:37:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -103,29 +103,29 @@ def game_hash
   }
 end
 
-def num_points_scored(player_search)
+def num_points_scored(player_name)
   game_hash.each do |team, team_info|
     # binding.pry
     team_info[:players].each do |player|
-      if player[:player_name] == player_search
+      if player[:player_name] == player_name
         return player[:points]
       end
     end
   end
 end
 
-def shoe_size(name)
+def shoe_size(player_name)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[:player_name] == name
+      if player[:player_name] == player_name
         return player[:shoe]
       end
     end
   end
 end
 
-def team_colors(team_input)
-  if team_input.downcase == "charlotte hornets" 
+def team_colors(team_name)
+  if team_name.downcase == "charlotte hornets" 
     return game_hash[:away][:colors]
   else return game_hash[:home][:colors]
   end
