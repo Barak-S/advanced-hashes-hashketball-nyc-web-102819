a17157fcbf97a70bed3ce06bdc100668db2539d2
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-26 20:26:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -105,6 +105,7 @@ end
 
 def num_points_scored(player_search)
   game_hash.each do |team, team_info|
+    # binding.pry
     team_info[:players].each do |player|
       if player[:player_name] == player_search
         return player[:points]
